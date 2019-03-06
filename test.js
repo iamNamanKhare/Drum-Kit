df37378baad9da60f4ca408d46ca6795fc2783c0
nmnkhr2509 <namankhare42@gmail.com>
2019-03-06 22:32:54 +0530

    Added All Essential Files for the Project

---
+++ test.js
@@ -0,0 +1,23 @@
+//For removing Transition
+function removeTransition(e) {
+    if (e.propertyName !== "transform")
+        return;
+    e.target.classList.remove('playing');
+}
+
+
+function playSound(e) {
+
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+    key.classList.add('playing');
+    if(!audio)return;
+    audio.currentTime = 0;
+	audio.play();
+	console.log(e);	
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key=>key.addEventListener('transitionend',removeTransition));
+
+window.addEventListener('keydown',playSound);
